1009bfbf6a8fcfe25abde86c17137223a1b0535d
slwu89 <slwu89@users.noreply.github.com>
2024-04-08 07:53:18 -0700

    ch 3 exercises

--- func-prog/3-interlude.lean
+++ func-prog/3-interlude.lean
@@ -18,7 +18,7 @@ def thirdOption (xs : List α) : Option α := xs[2]?
 
 #eval thirdOption ["only", "two"]
 
--- exercise 1a
+-- exercise 1
 def Ex1a : 2 + 3 = 5 := rfl
 #check Ex1a
 
@@ -36,3 +36,17 @@ def Ex1c : "Hello, ".append "world" = "Hello, world" := rfl
 theorem Ex1d : 5 < 18 := by
   simp
 #check Ex1d
+
+-- exercise 2
+def Ex2a : 2 + 3 = 5 := by simp
+#check Ex2a
+
+def Ex2b : 15 - 8 = 7 := by simp
+#check Ex2b
+
+def Ex2c : "Hello, ".append "world" = "Hello, world" := by simp
+#check Ex2c
+
+-- exercise 3
+def fifth (xs : List α) (ok : xs.length > 4) : α := xs[4]
+#eval fifth ["a", "b", "c", "d", "e"] (by simp)
